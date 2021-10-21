0d5ac45fcb2e541237213abacb7070e3fcbf0cf7
MrZebarth <kris.zebarth@ocsb.ca>
2021-10-21 11:13:06 -0400

    Skeleton Completed

--- TriangleSolver/src/trianglesolver/Main.java
+++ TriangleSolver/src/trianglesolver/Main.java
@@ -1,9 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This is the main program for the Triangle Solver. 
+ * You do not need to modify anything in this program. It will read in the measurements,
+ * set up the triangle, and call the solve and print methods.
+ * It is up to you to get the Triangle class fully solved.
  */
 package trianglesolver;
+
+import java.util.*;
 
 /**
  *
@@ -14,11 +17,87 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    public static Triangle t;
     public static void main(String[] args) {
-        // TODO code application logic here
-        Triangle t = new Triangle();
-        t.setAngleA(55);
-        System.out.println(t.getAngleA());
+        Scanner in = new Scanner(System.in);
+        //Read in three measurements
+        t=new Triangle();
+        String[] ord={"first","second","third"};
+        System.out.println("Welcome to the Triangle Solver!\n");
+        System.out.println("Do you want to see the instructions? y/n");
+        char choice = in.nextLine().toLowerCase().charAt(0);
+        if (choice=='y'){
+            Instructions();
+        }
+        for (int i=0;i<3;i++){
+            System.out.println("Enter the "+ord[i]+" line:");
+            StringTokenizer line = new StringTokenizer(in.nextLine());
+            if (!Update(line.nextToken(), Double.parseDouble(line.nextToken()))){
+                i--;
+            }
+        }
+        
+        t.solve();
+        t.printOut();
+
     }
     
+    public static void Instructions(){
+        System.out.println("\nIn order to solve a triangle, you need exactly 3 measurements");
+        System.out.println("The possibilities are A, B, C for angles and a, b, c for sides.");
+        System.out.println("Enter a letter, a space and a decimal number");
+        System.out.println("For example:\n\tA 55\n\tb 4\n\tc 5\n");
+        System.out.println("If you enter three angles, you will not get a valid response.");
+    }
+    
+    public static boolean Update(String name, double value){
+        switch (name){
+            case "a":
+                if (t.getSideA()!=0){
+                    System.out.println("Side a already set");
+                    return false;
+                }
+                t.setSideA(value);
+                break;
+            case "b":
+                if (t.getSideB()!=0){
+                    System.out.println("Side b already set");
+                    return false;
+                }
+                t.setSideB(value);
+                break;
+            case "c":
+                if (t.getSideC()!=0){
+                    System.out.println("Side c already set");
+                    return false;
+                }
+                t.setSideC(value);
+                break;
+            case "A":
+                if (t.getAngleA()!=0){
+                    System.out.println("Angle A already set");
+                    return false;
+                }
+                t.setAngleA(value);
+                break;
+            case "B":
+                if (t.getAngleB()!=0){
+                    System.out.println("Angle B already set");
+                    return false;
+                }
+                t.setAngleB(value);
+                break;
+            case "C":
+                if (t.getAngleC()!=0){
+                    System.out.println("Angle C already set");
+                    return false;
+                }
+                t.setAngleC(value);
+                break;
+            default:
+                System.out.println("That was not a valid measurement");
+                return false;
+        }
+        return true;
+    }
 }
--- TriangleSolver/src/trianglesolver/Triangle.java
+++ TriangleSolver/src/trianglesolver/Triangle.java
@@ -11,17 +11,18 @@ package trianglesolver;
  */
 public class Triangle {
     //attributes
-    
+
     //Sides
     public double sideA;
     private double sideB;
     private double sideC;
-    
+
     //Angles
     private double angleA;
     private double angleB;
     private double angleC;
 
+    //Getters and Setters
     public double getSideA() {
         return sideA;
     }
@@ -67,13 +68,176 @@ public class Triangle {
     }
 
     public void setAngleC(double angleC) {
-        if (angleC>0&&angleC<180){
+        if (angleC > 0 && angleC < 180) {
             this.angleC = angleC;
         }
     }
-    
-    //Sin law to find an angle given a, b, A, return B
-    public double sinAngle(double a, double b, double A){
-        return 0;
+
+    /**
+     * Analyze all combinations of sides and angles and find the missing parts
+     * if possible
+     */
+    public void solve() {
+        //3 angles
+        if (angleA != 0 && angleB != 0 && angleC != 0) {
+            System.out.println("Not possible to solve this!");
+            this.sideA = -1;
+            this.sideB = -1;
+            this.sideC = -1;
+            return;
+        }
+        //2 angles, 1 side
+        if (angleA > 0 && angleB > 0) {
+            //find angle C
+            if (sideA>0){
+                //find side b
+                //find side c
+            }else if (sideB>0){
+                //find side a
+                //find side c
+            }else if (sideC>0){
+                //find side a
+                //find side b
+            }
+        } else if (angleA > 0 && angleC > 0) {
+            //find angle B
+            if (sideA>0){
+                //find side b
+                //find side c
+            }else if (sideB>0){
+                //find side a
+                //find side c
+            }else if (sideC>0){
+                //find side a
+                //find side b
+            }
+        } else if (angleB > 0 && angleC > 0) {
+            //find angle A
+            if (sideA>0){
+                //find side b
+                //find side c
+            }else if (sideB>0){
+                //find side a
+                //find side b
+            }else if (sideC>0){
+                //find side a
+                //find side b
+            }
+        }
+        //1 angle, 2 sides
+        if (sideA > 0 && sideB > 0) {
+            if (angleA>0){
+                //find angle B
+                //find angle C
+                //find side c
+            }else if (angleB>0){
+                //find angle A
+                //find angle C
+                //find side c
+            }else if (angleC>0){
+                //find side c
+                //find angle A
+                //find angle B
+            }
+        }else if (sideA>0 && sideC>0){
+            if (angleA>0){
+                //find angle C
+                //find angle B
+                //find side B
+            }else if (angleB>0){
+                //find side B
+                //find angle A
+                //find angle C
+            }else if (angleC>0){
+                //find angle A
+                //find angle B
+                //find side B
+            }
+        }else if (sideB>0 && sideC>0){
+            if (angleA>0){
+                //find side A
+                //find angle B
+                //find angle C
+            }else if (angleB>0){
+                //find angle C
+                //find angle A
+                //find side A
+            }else if (angleC>0){
+                //find angle B
+                //find angle A
+                //find side A
+            }
+        }
+        //3 sides
+        if (sideA>0 && sideB>0&&sideC>0){
+            //find angle A
+            //find angle B
+            //find angle C
+        }
+    }
+
+    /**
+     * Print out all angles and sides
+     */
+    public void printOut() {
+        System.out.println("\nTriangle Measurements:\nAngles");
+        System.out.println("\tA\t" + this.angleA);
+        System.out.println("\tB\t" + this.angleB);
+        System.out.println("\tC\t" + this.angleC);
+        System.out.println("Sides");
+        System.out.println("\ta\t" + this.sideA);
+        System.out.println("\tb\t" + this.sideB);
+        System.out.println("\tc\t" + this.sideC);
+    }
+
+    /**
+     * Sin law to find an angle given a, b, A, return B
+     */
+    public double sinAngle(double a, double b, double A) {
+        double B = 0;
+        //A to radians
+        //B = sin(A)*b/a
+        //B to degrees
+        return B;
+    }
+
+    /**
+     * Sin law to find a side given a, A, B, return b
+     */
+    public double sinSide(double a, double A, double B) {
+        double b = 0;
+        //A to radians
+        //B to radians
+        //b = a*sin(B)/sin(A)
+        return b;
+    }
+
+    /**
+     * cos law to find an angle given a,b,c, return A
+     */
+    public double cosAngle(double a, double b, double c) {
+        double A = 0;
+        //A = acos((a^2-b^2-c^2)/(-2bc))
+        //A to degrees
+        return A;
+    }
+
+    /**
+     * cos law to find a side given a, b, C, return c
+     */
+    public double cosSide(double a, double b, double C) {
+        double c = 0;
+        //C to radians
+        //c = sqrt(a^2+b^2 - 2ab*cosC)
+        return c;
+    }
+
+    /**
+     * Interior angles. 180 - a - b. Return c
+     */
+    public double interiorAngle(double a, double b) {
+        double c = 0;
+        //c=180-a-b
+        return c;
     }
 }
